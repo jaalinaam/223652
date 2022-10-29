d4f31cc4de1f3d592c3fb314b40ec24249949f13
Jaali Naam <jaali.naam@protonmail.com>
2022-10-29 02:47:34 -0400

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
 hwclock --systohc
-#Edit /etc/locale.gen and uncomment en_US.UTF-8 UTF-8 and other needed locales. Generate the locales by running:
 locale-gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
+#Grub Install
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
+grub-mkconfig -o /boot/grub/grub.cfg
