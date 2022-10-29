4819600b403b6910eea28dbbcc324d8d3b0fcfc5
Jaali Naam <jaali.naam@protonmail.com>
2022-10-29 02:48:53 -0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -7,7 +7,7 @@ mount /dev/nvme1n1p2 /mnt
 mkdir -p /mnt/boot/efi
 swapon /dev/sdb3
 mount /dev/nvme1n1p1 /mnt/boot/efi
-pacstrap -K /mnt base linux linux-firmware vim grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel headers git cups ntfs-3g nvidia nvidia-utils packagekit-qt5 firefox curl wget xorg plasma kde-applications
+pacstrap /mnt base linux linux-firmware vim grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel headers git cups ntfs-3g nvidia nvidia-utils packagekit-qt5 firefox curl wget xorg plasma kde-applications
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "Done"
 echo "arch-chroot /mnt and run 2"
