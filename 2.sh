28092ee760f0f159a08339fd1c94f225981a5ab5
Jaali Naam <jaali.naam@protonmail.com>
2022-10-30 00:47:11 -0400

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
 hwclock --systohc
-locale-gen
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "arch" > /etc/hostname
 #Grub Install
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
+grub-install
+# Use below in case of issues
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager
 systemctl enable sddm.service
